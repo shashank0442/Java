9baebded8bfba2d70d14731640843ebe64795272
Shashank Kelkar <shashank.0442@gmail.com>
2021-08-07 17:57:43 +0530

    Encapsulation changes

--- Learn_Java/src/main/java/oops/encapsulation/Encapsulation.java
+++ Learn_Java/src/main/java/oops/encapsulation/Encapsulation.java
@@ -1,3 +1,15 @@
 package oops.encapsulation;
+
 public class Encapsulation {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
 }
---
+++ Learn_Java/src/main/java/oops/encapsulation/Test.java
@@ -0,0 +1,13 @@
+package oops.encapsulation;
+
+public class Test {
+
+    public static void main(String[] args) {
+        Encapsulation obj = new Encapsulation();
+        obj.setName("Shashank");
+        System.out.println(obj.getName());
+
+        obj.setName("Anay");
+        System.out.println(obj.getName());
+    }
+}
